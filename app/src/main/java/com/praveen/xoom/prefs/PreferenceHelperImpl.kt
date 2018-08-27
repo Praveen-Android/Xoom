29687be80e973b5d14f414d75a45751b3b71ce95
Praveen Kumar Godala <praveen.k.godala@kp.org>
2018-08-27 10:06:15 -0700

    minor changes that were missed in previous checkin

--- app/src/main/java/com/praveen/xoom/prefs/PreferenceHelperImpl.kt
+++ app/src/main/java/com/praveen/xoom/prefs/PreferenceHelperImpl.kt
@@ -17,19 +17,31 @@ class PreferenceHelperImpl @Inject constructor(context: Context): PreferenceHelp
         mPrefs = context.getSharedPreferences(APPLICATION_PREFERENCES_KEY, Context.MODE_PRIVATE)
     }
 
+    /**
+     * From the docs, we cannot modify the result of getStringSet() directly and consistency wont be guaranteed if we do so,
+     * so using a temp variable to store the favorites and re-inserting them to preferences with the changes we made (in this case add a favorite)
+     */
     override fun saveFavorite(countryCode: String) {
         val favorites = mPrefs.getStringSet(PREF_KEY_FAVORITES, mutableSetOf())
-        favorites.add(countryCode)
-        mPrefs.edit().putStringSet(PREF_KEY_FAVORITES, favorites).apply()
+        val data = favorites
+        data.add(countryCode)
+        mPrefs.edit().remove(PREF_KEY_FAVORITES).apply()
+        mPrefs.edit().putStringSet(PREF_KEY_FAVORITES, data).apply()
     }
 
     override fun fetchFavorites(): Set<String> {
         return mPrefs.getStringSet(PREF_KEY_FAVORITES, mutableSetOf())
     }
 
+    /**
+     * From the docs, we cannot modify the result of getStringSet() directly and consistency wont be guaranteed if we do so,
+     * so using a temp variable to store the favorites and re-inserting them to preferences with the changes we made (in this case remove a favorite)
+     */
     override fun deleteFavorite(countryCode: String) {
         val favorites = mPrefs.getStringSet(PREF_KEY_FAVORITES, mutableSetOf())
-        favorites.remove(countryCode)
-        mPrefs.edit().putStringSet(PREF_KEY_FAVORITES, favorites).apply()
+        val data = favorites
+        data.remove(countryCode)
+        mPrefs.edit().remove(PREF_KEY_FAVORITES).apply()
+        mPrefs.edit().putStringSet(PREF_KEY_FAVORITES, data).apply()
     }
 }
